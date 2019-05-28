3246482faaebbe698bf66959d5c3bb91b351323c
maguangxiao <maguangxiao@foxmail.com>
2019-05-28 19:56:15 +0800

    增加formData对象上传文件的脚本

--- postStudy/ajax/login.js
+++ postStudy/ajax/login.js
@@ -104,3 +104,30 @@ function getFormInfoWithData(){
       }
     });
 }
+
+
+//=================FormData对象
+function loadWithFormData()
+{
+  var form = document.forms.namedItem("fileinfo");
+  form.addEventListener('submit', function(ev) {
+
+    var oOutput = document.querySelector("div"),
+        oData = new FormData(form);
+
+    oData.append("CustomField", "This is some extra data");
+
+    var oReq = new XMLHttpRequest();
+    oReq.open("POST", "https://www.easy-mock.com/mock/5cc179f879522a0fe12bcda1/post/post/test/login", true);
+    oReq.onload = function(oEvent) {
+      if (oReq.status == 200) {
+        oOutput.innerHTML = "Uploaded!";
+      } else {
+        oOutput.innerHTML = "Error " + oReq.status + " occurred when trying to upload your file.<br \/>";
+      }
+    };
+
+    oReq.send(oData);
+    ev.preventDefault();
+  }, false);
+}
